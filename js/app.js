5c9c415534f07fbe8974391b0aa298ef13c9d630
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-13 21:52:47 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -13,17 +13,31 @@ for (let i = 0; i < 201; i++){
 }
 
 //excited kitten
-for (let i = 1; i < 21; i++){
-	console.log(i);
-	
+for (let i = 0; i < 20; i++){
 	if (i % 2 === 0){
 		let random = Math.floor(Math.random() * 91)
 		if (random < 30){
-			console.log("...human...why you taking pictures of me?...")
+			console.log(i, "...human...why you taking pictures of me?...")
 		} else if ((random >= 30) && (random < 60)){
-			console.log("...the catnip made me do it...")
+			console.log(i, "...the catnip made me do it...")
 		} else {
-			console.log("...why does the red dot always get away...")
-		}
-	} 
+			console.log(i, "...why does the red dot always get away...")
+		} 
+	} else{
+		console.log(i);
+	}
+}
+
+//Fizz Buzz
+
+for (let i = 1; i < 101; i++){
+
+if((i%3===0) && (i%5===0)){
+ console.log("FizzBuzz");
+} else if (i%3 === 0) {
+ console.log("Fizz")
+} else if (i%5 === 0) {
+ console.log("Buzz")
+} else {
+	console.log(i)
 }
