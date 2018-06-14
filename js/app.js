0560623e16cce549a509b0597153cfe13dd6376a
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-13 22:54:53 -0600

    Commit 7 - Kristyn and Thom have their outfits ready for class - array practice

--- js/app.js
+++ js/app.js
@@ -66,4 +66,84 @@ for(let i = 0; i < ninjaTurtles.length; i++){
 	console.log(ninjaTurtles[i].toUpperCase());
 }
 
+//Return of the Closet
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+]; 
+
+//alien attire
+
+const kristynShoe = kristynsCloset.splice(0,1);
+console.log(kristynShoe);
+
+thomsCloset[2].push(kristynShoe[0]);
+console.log(thomsCloset)
+
+//I had trouble adding the left shoe into thomsCloset as just the value and not the array
+
+//Dress Us Up
+
+
+//Kristyn
+const kristynOutfit1 = []
+kristynOutfit1.push(kristynsCloset[0], kristynsCloset[3], kristynsCloset[6]);
+
+console.log(`Kristyn's outfit tonight will be a ${kristynOutfit1[0]}, ${kristynOutfit1[1]}, and a ${kristynOutfit1[2]}`)
+
+const kristynOutfit2 = []
+kristynOutfit2.push(kristynsCloset[1], kristynsCloset[4], kristynsCloset[5]);
+
+console.log(`Kristyn's second outfit tonight will be ${kristynOutfit2[0]}, ${kristynOutfit2[1]}, and a ${kristynOutfit2[2]}`)
+
+const kristynOutfit3 = []
+kristynOutfit3.push(kristynsCloset[3], kristynsCloset[4], kristynsCloset[5]);
+
+console.log(`Kristyn's third outfit tonight will be a ${kristynOutfit3[0]}, ${kristynOutfit3[1]}, and a ${kristynOutfit3[2]}`)
+
+//Thom
+
+ const thomsOutfit1 = []
+thomsOutfit1.push(thomsCloset[0][1], thomsCloset[2][0], thomsCloset[0][2]);
+
+console.log(`Thom's first outfit tonight will be a ${thomsOutfit1[0]}, ${thomsOutfit1[1]}, and a ${thomsOutfit1[2]}`)
+
+const thomsOutfit2 = []
+thomsOutfit2.push(thomsCloset[1][0], thomsCloset[0][2], thomsCloset[2][2]);
+
+console.log(`Thom's second outfit tonight will be ${thomsOutfit2[0]}, a ${thomsOutfit2[1]}, and ${thomsOutfit2[2]}`)
+
+const thomsOutfit3 = []
+thomsOutfit3.push(thomsCloset[2][1], thomsCloset[0][0], thomsCloset[1][2]);
+
+console.log(`Thom's third outfit tonight will be a ${thomsOutfit3[0]}, a ${thomsOutfit3[1]}, and ${thomsOutfit3[2]}`)
+
+
 
