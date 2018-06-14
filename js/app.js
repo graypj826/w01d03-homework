f8e4d54c515ee9f4983ccd19dfff64d1e95b3a5f
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-13 21:45:17 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -12,3 +12,18 @@ for (let i = 0; i < 201; i++){
 }
 }
 
+//excited kitten
+for (let i = 1; i < 21; i++){
+	console.log(i);
+	
+	if (i % 2 === 0){
+		let random = Math.floor(Math.random() * 91)
+		if (random < 30){
+			console.log("...human...why you taking pictures of me?...")
+		} else if ((random >= 30) && (random < 60)){
+			console.log("...the catnip made me do it...")
+		} else {
+			console.log("...why does the red dot always get away...")
+		}
+	} 
+}
